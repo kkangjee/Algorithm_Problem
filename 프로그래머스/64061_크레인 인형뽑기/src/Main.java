e950d40a8f4cd4f1c9bbefd36b4e9520eda79289
kkangjee <wlgns337@gmail.com>
2020-12-27 22:46:00 +0900

    프로그래머스 추가

---
+++ 프로그래머스/64061_크레인 인형뽑기/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+        int[] moves = {1,5,3,5,1,2,1,4};
+        Solution solution = new Solution();
+        solution.solution(board,moves);
+
+        System.out.println(board);
+        System.out.println(moves);
+    }
+}
---
+++ 프로그래머스/64061_크레인 인형뽑기/src/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+
+        for(int row = 0;row<board.length;row++){
+            for(int col = 0;col<board.length;col++){
+                //if(board[row][col])
+            }
+        }
+
+
+        // System.out.println(board);
+        // System.out.println(moves);
+        return answer;
+    }
+}
