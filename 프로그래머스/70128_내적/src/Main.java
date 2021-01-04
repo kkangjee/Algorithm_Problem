9b6843f72dd3ffb567cf94e5fc11e2842be1722b
kkangjee <wlgns337@gmail.com>
2021-01-04 17:17:36 +0900

    1문제 추가

---
+++ 프로그래머스/70128_내적/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4};
+        int[] b = {-3, -1, 0, 2};
+
+        Solution solution = new Solution();
+        solution.solution(a, b);
+
+        //System.out.println(board);
+        //System.out.println(moves);
+    }
+}
