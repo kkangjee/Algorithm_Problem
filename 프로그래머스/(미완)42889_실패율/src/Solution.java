8ac18f3d2c977d71a0f2affedeac44a2ea5f5864
kkangjee <wlgns337@gmail.com>
2021-01-12 16:21:45 +0900

    1문제 성공 2문제 미완

--- 프로그래머스/(미완)42889_실패율/src/Solution.java
+++ 프로그래머스/(미완)42889_실패율/src/Solution.java
@@ -20,28 +20,51 @@ class Solution {
                 i--;
                 temp++;
             }
-            //System.out.println(stages[i] + " " + Arrays.toString(fail));
         }
-        System.out.println(Arrays.toString(fail));
+        //System.out.println(Arrays.toString(fail));
 
-        Map map = new HashMap();
+        Map map = new HashMap<Integer,Double>();
         for (int i = 0, j = stages.length; i < fail.length; i++) {
-            System.out.println(fail[i]+" / "+j+" = "+  fail[i]/j);
+            //System.out.println(fail[i] + " / " + j + " = " + fail[i] / j);
 
             int a = (int) fail[i];
             fail[i] /= j;
-            map.put(i+1,fail[i]);
+            map.put(i + 1, fail[i]);
             j -= a;
 
         }
 
-        System.out.println(Arrays.toString(fail));
+        //System.out.println(Arrays.toString(fail));
+        System.out.println(map);
+        //System.out.println(map.get(2));
+        double max = 0;
+        int big = 0;
 
+        for (int i = 1; i <= map.size(); i++) {
+            if ((double) map.get(i) > max) {
+                max = (double) map.get(i);
+                big = i;
+            }
+            System.out.println(i);
+        }
 
-        Object[] mapkey = map.values().toArray();
-        Arrays.sort(mapkey, Collections.reverseOrder());
+        for (int j = 0; j < answer.length; j++) {
+            for (int i = 1; i <= map.size(); i++) {
+                if ((double) map.get(i) > max) {
+                    max = (double) map.get(i);
+                    big = i;
+                }
+            }
+            answer[j]= big;
+            map.remove(big);
+            max = 0;
+        }
 
-        System.out.println(Arrays.toString(mapkey));
+        System.out.println(answer);
+//        Object[] mapkey = map.values().toArray();
+//        Arrays.sort(mapkey, Collections.reverseOrder());
+//
+//        System.out.println(Arrays.toString(mapkey));
 //        double max = 0.0;
 //
 //        for (int i = 0; i < fail.length; i++) {
--- 프로그래머스/17682_다트 게임/src/Main.java
+++ 프로그래머스/17682_다트 게임/src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        String str = "1S2D3T";
-        //String str = "0D0D0S*";
+        //String str = "1S2D*3T";
+        String str = "1D2S#10S*";
         Solution solution = new Solution();
         solution.solution(str);
+
 
 
     }
